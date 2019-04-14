affd07da1d5c387661f141cb566acc39f47b6139
Jimmy <yebdou@gmail.com>
2019-04-14 14:02:19 +0300

    0.5.29

--- index.js
+++ index.js
@@ -66,13 +66,13 @@ const {
 } = require("./utils/linters/prettier");
 
 // ********** Version **********
-program.version("v0.5.27", "-v, --version");
+program.version("v0.5.29", "-v, --version");
 
 program
   .command("version")
   .description("Print Omnilint version.")
   .action(() => {
-    console.log("v0.5.27");
+    console.log("v0.5.29");
     process.exit(0);
   });
 
